4923f900c4140ce795380a44c60b88884437b9a7
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-12 12:37:10 -0500

    created questions, storing the responses

---
+++ ex1.py
@@ -0,0 +1,13 @@
+documentary = 'FYRE: The Greatest Party That Never Happened'
+drama = 'The Shawshank Redemption'
+comedy = 'BASEketball'
+dramedy = 'The Truman Show'
+
+print('Do you like documentaries?')
+doc_response = input().lower()
+print('Do you like dramas?')
+drama_response = input().lower()
+print('Do you like comedies?')
+com_response = input().lower()
+
+print(doc_response + ' ' + drama_response + ' ' + com_response)
